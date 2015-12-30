5288a8ac6868469dd4b8fd74fe92ae7dd493cd48
Zain Z <contact@zainzulfiqar.com>
2015-12-30 17:12:26 -0500

    Initial commit

---
+++ app/src/main/java/com/zainzulfiqar/newsrails/main.java
@@ -0,0 +1,15 @@
+package com.zainzulfiqar.newsrails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
